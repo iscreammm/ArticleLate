8f458659a7a37f3fe62ce21eeed7b2be697aa3a9
Exonor515 <exonor.work@gmail.com>
2022-11-08 22:20:53 +0300

    initialize client

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import root from "../index";
+
+const App = () => {
+
+  return (
+    <div className="main">
+      <div className="wrapper">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App
