17c60d1b08e1547e211df3df6179444e1cfdb072
naso137 <nasonov137@bk.ru>
2022-11-18 20:43:55 +0300

    Add News page markup and styles

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,10 +1,22 @@
 import { Outlet } from 'react-router-dom';
+import "../styles/layout.css";
 
 const Layout = () => {
   return (
-    <>
+    <div>
+      <div className='menu'>
+        <img className='avatar' src="layout/avatar.png" alt="Avatar" /> 
+        <p className='notificationsContainer'> 
+          <img className='notificationsIcon' src="layout/bell.PNG" alt="Bell" />  
+          <p className='notificationsCount'>0</p>
+        </p>
+        <img className='menuButton' src="layout/news.PNG" alt="News" style={{margin: '2rem 0 0.7rem'}} />  
+        <img className='menuButton' src="layout/subs.PNG" alt="Subs" />      
+      </div>
+      <img className='logoMenu' src="common/logo.PNG" alt="Logo" />
+      <img className='exit menuButton' src="layout/exit.PNG" alt="Exit" />
       <Outlet />
-    </>
+    </div>
   );
 
 }
