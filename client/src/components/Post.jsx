1e74c35179b6dd14c1ade9ecd126042360e4036f
naso137 <nasonov137@bk.ru>
2022-11-19 18:34:13 +0300

    Add post markup

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -3,7 +3,30 @@ import "../styles/feedPosts.css";
 const Post = () => {
   return (
     <div className="postContent">
-      asddas
+      <div className="postInfo">
+        <div className="postUserInfo">
+          <img src="post/avatarcircle.png" alt="AvatarCircle" />
+          <div style={{marginTop: "0.15vw", textAlign: "center"}}>
+            <p style={{fontSize: '1.2em'}}>Аниматор</p>
+            <p>@animator2048</p>
+          </div>
+        </div>
+        <div className="postDate">
+          <p>19.11.2022 17:11</p>
+          <p>Игры</p>
+        </div>
+      </div>
+      <div className="postMainContent">
+        hgjkl;''
+        <img src="post/imagepost.png" alt="ImagePost" />
+      </div>
+      <div className="postBottom">
+        <div className="likeContainer">
+          <img src="post/whitelike.png" alt="WhiteLike" />
+          <p>0</p>
+        </div>
+        <button>Комментировать</button>
+      </div>
     </div>
   );
 }
