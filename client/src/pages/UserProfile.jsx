cc834c72177eeed664db711b4876e189ee7326e4
naso137 <nasonov137@bk.ru>
2022-11-22 16:50:09 +0300

    update profile styles

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -6,16 +6,20 @@ const UserProfile = () => {
     <div className="pageContainer profile">
       <div className="profileInfo">
         <div className="infoColumn">
-          <p className="infoText">Аноним</p>
-          <p className="infoText">@anonim123</p>
+          <div className="infoText">
+            <p>Аноним</p>
+            <p>@anonim123</p>
+          </div>
         </div>
         <div className="infoColumn">
           <img className="userAvatar" src="layout/avatar.png" alt="Avatar2" />
           <img className="profileInfoButton" src="profile/change.png" alt="Change" />
         </div>
         <div className="infoColumn">
-          <p className="infoText">Подписки: 0</p>
-          <p className="infoText">Подписчики: 0</p>
+          <div className="infoText">
+            <p>Подписки: 0</p>
+           <p>Подписчики: 0</p>
+          </div>
         </div>
       </div>
       <p className="about">Анонимный пользователь сайта, который сидит тут и не знает, что написать о себе.</p>
