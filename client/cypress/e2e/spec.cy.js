901009c388aab9082db40a15260a15281141ab23
Exonor515 <exonor.work@gmail.com>
2023-04-08 02:22:53 +0300

    add expects in likes test

--- client/cypress/e2e/spec.cy.js
+++ client/cypress/e2e/spec.cy.js
@@ -235,18 +235,18 @@ describe("e2e tests", () => {
       secondButton.click();
     });
 
-    cy.get('[alt="Like"]').eq(0).should('have.attr', 'src').should('include', 'redlike.png');
-    cy.get('[alt="Like"]').eq(2).should('have.attr', 'src').should('include', 'redlike.png');
+    expect(cy.get('[alt="Like"]').eq(0).should('have.attr', 'src').should('include', 'redlike.png'));
+    expect(cy.get('[alt="Like"]').eq(2).should('have.attr', 'src').should('include', 'redlike.png'));
 
     cy.get('[alt="Like"]').eq(0).click();
 
-    cy.get('[alt="Like"]').eq(0).should('have.attr', 'src').should('include', 'whitelike.png');
-    cy.get('[alt="Like"]').eq(2).should('have.attr', 'src').should('include', 'whitelike.png');
+    expect(cy.get('[alt="Like"]').eq(0).should('have.attr', 'src').should('include', 'whitelike.png'));
+    expect(cy.get('[alt="Like"]').eq(2).should('have.attr', 'src').should('include', 'whitelike.png'));
 
     cy.get('[alt="Like"]').eq(0).click();
 
-    cy.get('[alt="Like"]').eq(0).should('have.attr', 'src').should('include', 'redlike.png');
-    cy.get('[alt="Like"]').eq(2).should('have.attr', 'src').should('include', 'redlike.png');
+    expect(cy.get('[alt="Like"]').eq(0).should('have.attr', 'src').should('include', 'redlike.png'));
+    expect(cy.get('[alt="Like"]').eq(2).should('have.attr', 'src').should('include', 'redlike.png'));
   });
 
   it("Shouldn't modify user data after click on decline btn", () => {
