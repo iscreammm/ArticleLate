9f11c004fbfe949c72f7dddd924e0e5b0faa8de0
naso137 <nasonov137@bk.ru>
2023-04-08 13:28:31 +0300

    Revert "tests optimizing"
    This reverts commit 267007ac48dd8f7f3799386ed810c564f672ad32.

--- client/cypress/e2e/spec.cy.js
+++ client/cypress/e2e/spec.cy.js
@@ -224,6 +224,35 @@ describe("e2e tests", () => {
     cy.findByText(postData3.postText).should('not.exist');
   });
 
+  it("Should successfully refresh likes in post and comment modal of that post", () => {
+    login(user1.loginData);
+
+    cy.findByAltText("Avatar").click();
+
+    cy.get('[alt="Like"]').eq(1).click();
+
+    cy.findAllByRole('button', { name: 'Комментировать'}).spread((firstButton, secondButton) => {
+      secondButton.click();
+    });
+
+    cy.get('[alt="Like"]').eq(0).should('have.attr', 'src', 'post/whitelike.png');
+    cy.get('[alt="Like"]').eq(2).should('have.attr', 'src', 'post/whitelike.png');
+
+    cy.get('[alt="Like"]').eq(0).click();
+
+    cy.wait(500);
+
+    cy.get('[alt="Like"]').eq(0).should('have.attr', 'src', 'post/whitelike.png');
+    cy.get('[alt="Like"]').eq(2).should('have.attr', 'src', 'post/whitelike.png');
+
+    cy.get('[alt="Like"]').eq(0).click();
+
+    cy.wait(500);
+    
+    cy.get('[alt="Like"]').eq(0).should('have.attr', 'src', 'post/redlike.png');
+    cy.get('[alt="Like"]').eq(2).should('have.attr', 'src', 'post/redlike.png');
+  });
+
   it("Shouldn't modify user data after click on decline btn", () => {
     login(user1.loginData);
 
